d9e5d630594ec1b986a8393ad33c2566ba8338e2
PatrykJankowski <patryk.jankowski@protonmail.com>
2021-10-31 09:39:23 +0100

    products pages updates

--- src/functions.php
+++ src/functions.php
@@ -204,3 +204,21 @@ function filter_function(){
 
     die();
 }
+
+
+
+/**
+ * Change number of related products output
+ */
+function woo_related_products_limit() {
+    global $product;
+
+    $args['posts_per_page'] = 6;
+    return $args;
+}
+add_filter('woocommerce_output_related_products_args', 'jk_related_products_args', 20);
+function jk_related_products_args( $args ) {
+    $args['posts_per_page'] = 5;
+    $args['columns'] = 5;
+    return $args;
+}
--- src/page-favourites.php
+++ src/page-favourites.php
@@ -7,7 +7,7 @@
         <img src="/wp-content/themes/zywnosc/img/przepisy.webp" style="width:100%">
         <div class="container container-slider">
             <div class="text">
-                <span>Ulubione przepisy i artykuły</span>
+                <h1>Ulubione przepisy i artykuły</h1>
             </div>
         </div>
     </div>
@@ -17,7 +17,6 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h2 class="mb-l">Lista ulubionych</h2>
                 <?php echo do_shortcode('[user_favorites include_thumbnails="true" include_excerpts="true"]'); ?>
             </div>
         </div>
